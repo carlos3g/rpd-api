02ff0ad4584894921b8333c08338862d9fea0242
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2024-07-18 12:44:21 -0300

    style: defragment test setup files

--- test/server.ts
+++ test/server.ts
@@ -1,8 +1,32 @@
 /* eslint-disable import/no-mutable-exports */
+import { AppModule } from '@app/app.module';
 import { PrismaService } from '@app/lib/prisma/services/prisma.service';
 import type { INestApplication } from '@nestjs/common';
-import { getApplication } from '@test/get-application';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Test } from '@nestjs/testing';
 import type { Server } from 'net';
+
+const createTestingModule = async () => {
+  const moduleFixture = Test.createTestingModule({
+    imports: [AppModule],
+  });
+
+  const compiled = await moduleFixture.compile();
+
+  const app = compiled.createNestApplication<INestApplication<Server>>();
+
+  return app;
+};
+
+const getApplication = async () => {
+  const app = await createTestingModule();
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  return app;
+};
 
 // see: https://github.com/nestjs/nest/issues/13191#issuecomment-1938178694
 export let app: INestApplication<Server>;
